bf41594d03846089f9f660f72e706cbb94182e9e
KanG98 <yankang198@gmail.com>
2025-01-23 20:39:07 -0500

    initial commit

---
+++ service/service-passkey/src/main/java/com/yankang198/demo/service/servicepasskey/PasskeyServiceApplication.java
@@ -0,0 +1,11 @@
+package com.yankang198.demo.service.servicepasskey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasskeyServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PasskeyServiceApplication.class, args);
+    }
+}
